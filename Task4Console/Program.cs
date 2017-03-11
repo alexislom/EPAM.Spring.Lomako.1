45f3eee971535ad70244519aee068ccc3877598e
Alex Lomako <alexislomako@gmail.com>
2017-03-11 22:10:18 +0300

    last changes i think...

--- Task4Console/Program.cs
+++ Task4Console/Program.cs
@@ -11,13 +11,10 @@ namespace Task4Console
     {
         static void Main(string[] args)
         {
-            //string s = "adasgafhafdaaagdfhbbbcvssdd";
-            //string s2 = new string(s.Distinct().ToArray());
-            //Console.WriteLine(s2);
             string str1 = "kkkkaaabbbccc";
-            string str2 = "aavvdddeee";
-            string sss = StringConcat.MergeTwoStrings(str1, str2);
-            Console.WriteLine(sss);
+            string str2 = "aavvdddeeegggiiijjjrrrtttasfsdgafh";
+            string result = StringConcat.MergeTwoStrings(str1, str2);
+            Console.WriteLine(result);
         }
     }
 }
--- Task4Logic/StringConcat.cs
+++ Task4Logic/StringConcat.cs
@@ -6,15 +6,24 @@ using System.Threading.Tasks;
 
 namespace Task4Logic
 {
+    /// <summary>
+    /// Class which contains method of merge two strings 
+    /// </summary>
+    /// <remarks>merge two strings without duplicate letters and in alphabetical order</remarks>
     public static class StringConcat
     {
+        /// <summary>
+        /// Merge two strings
+        /// </summary>
+        /// <param name="strOne">reference to the first string</param>
+        /// <param name="strTwo">reference to the second string</param>
+        /// <returns>new string</returns>
         public static string MergeTwoStrings(string strOne, string strTwo)
         {
             if (strOne == null || strTwo == null)
             {
-                throw new ArgumentNullException(strOne == null ? nameof(strOne) : nameof(strTwo));
+                throw new ArgumentNullException("You should use strings not null"); 
             }
-
             return new string((strOne + strTwo).Distinct().OrderBy(c => c).ToArray());
         }
     }
