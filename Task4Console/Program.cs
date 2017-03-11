e910342dba9ca131f036310b8ccef0082b6a4f3a
Alex Lomako <alexislomako@gmail.com>
2017-03-11 21:52:44 +0300

    Add the last task(solve in one string)

---
+++ Task4Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task4Logic;
+
+namespace Task4Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "adasgafhafdaaagdfhbbbcvssdd";
+            //string s2 = new string(s.Distinct().ToArray());
+            //Console.WriteLine(s2);
+            string str1 = "kkkkaaabbbccc";
+            string str2 = "aavvdddeee";
+            string sss = StringConcat.MergeTwoStrings(str1, str2);
+            Console.WriteLine(sss);
+        }
+    }
+}
---
+++ Task4Logic/StringConcat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4Logic
+{
+    public static class StringConcat
+    {
+        public static string MergeTwoStrings(string strOne, string strTwo)
+        {
+            if (strOne == null || strTwo == null)
+            {
+                throw new ArgumentNullException(strOne == null ? nameof(strOne) : nameof(strTwo));
+            }
+
+            return new string((strOne + strTwo).Distinct().OrderBy(c => c).ToArray());
+        }
+    }
+}
